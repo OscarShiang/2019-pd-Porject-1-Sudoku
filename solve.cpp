b74b31e60bfcc95733f6e631f3118b21916ab532
OscarShiang <oscar0225@livemail.tw>
2019-03-18 12:44:26 +0800

    Optimize the seed

--- solve.cpp
+++ solve.cpp
@@ -7,7 +7,7 @@ Sudoku sudoku;
 int pos = 0, cnt = 0;
 
 bool solve() {
-    int index = sudoku.getFirstZero(0);
+    int index = sudoku.getFirstZero(pos);
     if (index == -1) {
         cnt ++;
         sudoku.solved();
@@ -16,24 +16,16 @@ bool solve() {
     for (int i = 1; i <= 9; i ++) {
         if (sudoku.isSafe(index, i)) {
             sudoku.setElement(index, i);
+            pos = index;
             solve();
             sudoku.setElement(index, 0);
+            pos = index;
         }
     }
     return false;
 }
 
 int main (void) {
-    // int seed[] = {0, 0, 0, 5, 0, 1, 0, 0, 3,
-    //               6, 8, 0, 0, 0, 9, 5, 4, 0,
-    //               5, 0, 0, 7, 0, 0, 0, 0, 2,
-    //               7, 9, 8, 0, 0, 0, 0, 0, 0,
-    //               0, 6, 0, 3, 0, 8, 0, 9, 0,
-    //               0, 0, 0, 0, 0, 0, 8, 6, 5,
-    //               3, 0, 0, 0, 0, 7, 0, 0, 6,
-    //               0, 7, 2, 6, 0, 0, 0, 3, 9,
-    //               9, 0, 0, 1, 0, 3, 0, 0, 0};
-    // load the seed into sudoku object
     vector <int> board;
     board.assign(81, 0);
     for (int i = 0; i < sudoku.sudoku_size; i ++)
